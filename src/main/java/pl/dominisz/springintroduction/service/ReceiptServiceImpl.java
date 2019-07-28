f123f358766193cffc5779fdafd7d4548e2ff22a
dominisz <dominisz@gmail.com>
2019-07-28 09:38:47 +0200

    Receipt resource

--- src/main/java/pl/dominisz/springintroduction/service/ReceiptServiceImpl.java
+++ src/main/java/pl/dominisz/springintroduction/service/ReceiptServiceImpl.java
@@ -1,9 +1,10 @@
 package pl.dominisz.springintroduction.service;
 
+import org.springframework.beans.BeanUtils;
 import org.springframework.stereotype.Service;
-import pl.dominisz.springintroduction.model.CreateReceiptDto;
-import pl.dominisz.springintroduction.model.Receipt;
-import pl.dominisz.springintroduction.model.ReceiptDto;
+import pl.dominisz.springintroduction.exception.EntityNotFoundException;
+import pl.dominisz.springintroduction.model.*;
+import pl.dominisz.springintroduction.repository.OrderRepository;
 import pl.dominisz.springintroduction.repository.ReceiptRepository;
 
 import java.util.List;
@@ -14,10 +15,15 @@ public class ReceiptServiceImpl implements ReceiptService {
 
   private final BillingService billingService;
   private final ReceiptRepository receiptRepository;
+  private final OrderRepository orderRepository;
 
-  public ReceiptServiceImpl(BillingService billingService, ReceiptRepository receiptRepository) {
+  public ReceiptServiceImpl(
+      BillingService billingService,
+      ReceiptRepository receiptRepository,
+      OrderRepository orderRepository) {
     this.billingService = billingService;
     this.receiptRepository = receiptRepository;
+    this.orderRepository = orderRepository;
   }
 
   @Override
@@ -30,11 +36,25 @@ public class ReceiptServiceImpl implements ReceiptService {
   }
 
   private ReceiptDto convert(Receipt receipt) {
-    return null;
+    ReceiptDto dto = new ReceiptDto();
+    BeanUtils.copyProperties(receipt, dto);
+    return dto;
   }
 
   @Override
   public ReceiptDto createReceipt(CreateReceiptDto createReceiptDto) {
-    return null;
+    Order order =
+        orderRepository
+            .findById(createReceiptDto.getOrderId())
+            .orElseThrow(
+                () ->
+                    new EntityNotFoundException(
+                        "Order " + createReceiptDto.getOrderId() + "not found"));
+
+    CreditCard creditCard = order.getUser().getCreditCard();
+
+    Receipt receipt = billingService.chargeOrder(order, creditCard);
+
+    return convert(receiptRepository.save(receipt));
   }
 }
